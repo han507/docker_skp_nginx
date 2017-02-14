bcb07307d726559d8d2575ac4789079811cff8f4
moomoo <han507@hanmail.net>
2017-02-14 19:36:09 +0900

    asdf

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker rm -f skp_nginx > /dev/null 2>&1
-docker run -d -p 80:80 --name skp_ngin skp_nginx > /dev/null 2>&1
+docker run -d -p 80:80 --name skp_nginx skp_nginx > /dev/null 2>&1
